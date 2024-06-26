708acd632b1147ce89312c8dacebc3d073f8c7f8
EdgarRetes <edgar.retes24@gmail.com>
2024-06-25 18:19:17 -0600

    Cambios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-     <h1>rama de prueba</h1>
+     <h1>prendan sus camaras</h1>
     </main>
   );
 }
